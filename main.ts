b960d424774062b9342067e1c1251c67623c1588
bryan weinstein <bryanweinstein@outlook.com>
2020-11-05 15:48:15 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,59 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
+        // disarm the safe
         safeState = 0
+        opened = 0
         pins.servoWritePin(AnalogPin.P1, 0)
     } else {
+        // arm the safe
         safeState = 1
-        pins.servoWritePin(AnalogPin.P1, 180)
     }
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Armed")
-    radio.sendNumber(1)
-    basic.showIcon(IconNames.TShirt)
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showString("Safe")
+    // disarm the safe
     radio.sendNumber(0)
+    basic.showString("OFF")
     basic.showIcon(IconNames.Square)
 })
+input.onButtonPressed(Button.B, function () {
+    // arm the safe
+    radio.sendNumber(1)
+    basic.showString("Armed")
+    basic.showIcon(IconNames.TShirt)
+})
+let opened = 0
 let safeState = 0
 radio.setGroup(1)
 basic.forever(function () {
-    if (safeState == 1) {
-        // System has not been compromised so all is well - do nothing
-        // System is compromised so need to alert!
-        if (input.pinIsPressed(TouchPin.P0)) {
-            music.playMelody("C - - - - - - - ", 120)
-        } else {
-            while (true) {
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    # . # . .
-                    . . . . .
-                    . . . . .
-                    `)
-                basic.pause(100)
-                basic.showLeds(`
-                    . . . . .
-                    . . . . .
-                    . . # . #
-                    . . . . .
-                    . . . . .
-                    `)
-                basic.pause(100)
-                basic.clearScreen()
-            }
-        }
-    } else {
+    while (safeState == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(100)
         basic.clearScreen()
+        // System has not been compromised so all is well - do nothing
+        if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+            // System is compromised so need to alert!
+            safeState = 0
+            basic.showIcon(IconNames.No)
+            // System is compromised so need to alert!
+            opened = 1
+            // Safe is opened - write to server to show broken flag
+            pins.servoWritePin(AnalogPin.P1, 180)
+            // arm the safe
+            radio.sendNumber(2)
+        }
     }
 })
